57f8f364db0959f0d9f82ff75a57f556bcfb9e72
Diego Roberto Antunes <drantunes@gmail.com>
2021-09-29 10:46:37 -0300

    Estrutura inicial do projeto criada

---
+++ README.md
@@ -0,0 +1,3 @@
+# Como mudar a orientação da tela no Flutter?
+
+Este projeto é um exemplo simples de como alterar ou forçar a orientação da tela no Flutter, tanto para permanecer na horizontal quanto na vertical. Esta funcionalidade pode ser útil em aplicativos (e.g. jogos, vídeo, etc.) que precisam manter uma orientação específica. 
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_change_orientation/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
