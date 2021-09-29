b2581bdfbb8a77ba86aef5201337cb16b4ae8451
Diego Roberto Antunes <drantunes@gmail.com>
2021-09-29 15:13:21 -0300

    Exemplo finalizado

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,10 +10,32 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeRight]);
+  }
+
+  mudarOrientacao() {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Meu conteúdo horizontal'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.rotate_right,
+              size: 120,
+            ),
+            ElevatedButton(
+              onPressed: mudarOrientacao,
+              child: const Text('Mudar Orientação'),
+            ),
+          ],
+        ),
       ),
     );
   }
